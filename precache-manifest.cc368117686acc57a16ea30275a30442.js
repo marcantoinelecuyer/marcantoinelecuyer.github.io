30887139e9faabb0a68e3e17b989406a52850734
Marc-Antoine <marcantoine.lecuyer7@gmail.com>
2020-05-17 22:43:00 -0400

    init

---
+++ precache-manifest.cc368117686acc57a16ea30275a30442.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffefa3d9b71007f313c4fcb0477b930",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe212f62abdb3e9d1e40",
+    "url": "/static/css/main.886bc011.chunk.css"
+  },
+  {
+    "revision": "6cce963e24fb130d260d",
+    "url": "/static/js/2.e971ab0b.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/static/js/2.e971ab0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe212f62abdb3e9d1e40",
+    "url": "/static/js/main.55866faf.chunk.js"
+  },
+  {
+    "revision": "c5ede58802edeb1106e1",
+    "url": "/static/js/runtime-main.e881ee05.js"
+  },
+  {
+    "revision": "f0cf6e7500858baa6a053d11dcf9ecc4",
+    "url": "/static/media/a.f0cf6e75.png"
+  },
+  {
+    "revision": "1a4e336960ffa2b64311421d3b1ff1ac",
+    "url": "/static/media/beecomb.1a4e3369.png"
+  },
+  {
+    "revision": "afef525ee6eca874302b29369c0c5f47",
+    "url": "/static/media/grumpygoats.afef525e.png"
+  },
+  {
+    "revision": "67700ae3b0510df03de27edcff80d630",
+    "url": "/static/media/inked.67700ae3.png"
+  },
+  {
+    "revision": "02aa8b1eea5c4866c1b8928264443119",
+    "url": "/static/media/m.02aa8b1e.png"
+  },
+  {
+    "revision": "5df0803b3a5a861ccfb307663582795a",
+    "url": "/static/media/plague.5df0803b.png"
+  },
+  {
+    "revision": "225fff640486841ffaa6aadd31a07a60",
+    "url": "/static/media/siblants.225fff64.png"
+  },
+  {
+    "revision": "91d4e6854545e69343e6ce291fe3d18c",
+    "url": "/static/media/tactile.91d4e685.png"
+  },
+  {
+    "revision": "2622da7314f2f7adad813f5f29f05c00",
+    "url": "/static/media/tron.2622da73.png"
+  }
+]);
